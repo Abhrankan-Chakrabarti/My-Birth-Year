7b6a6403bcc3c744aa719effea0c5fa3d98f6f61
Abhrankan Chakrabarti <abhrankan@gmail.com>
2025-03-06 19:42:07 +0530

    Update BirthYearcalculator.py

--- BirthYearcalculator.py
+++ BirthYearcalculator.py
@@ -1,6 +1,7 @@
-# My-Birth-Year
-birth_year = input('Birth Year: ')#insert value
-print(type(birth_year))#converting string to int
-age = 2024  - int(birth_year)#calculation
-print(type(age))#convert string to int
-print(age)#printing age
+birth_year = input('Birth Year: ')  # Taking user input as a string
+print(type(birth_year))  # Printing the type of birth_year (it will be str)
+
+age = 2024 - int(birth_year)  # Converting birth_year to int and calculating age
+print(type(age))  # Printing the type of age (it will be int)
+
+print(age)  # Printing the calculated age
